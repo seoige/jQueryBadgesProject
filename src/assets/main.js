903d5db9e5180d240257e0a49217354f215228d3
Sean Joyce <sean.joyce@MacBook-Pro.local>
2017-01-13 14:56:40 +0000

    appended course divs to badges element

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
-
   // your code will go here
+  $.ajax({
+     url: 'https://www.codeschool.com/users/2764134.json',
+     dataType: 'jsonp',
+     success: function(response) {
+       // handle response
+       console.dir(response.courses.completed);
+       addCompletedCourses(response.courses.completed);
+     }
+   });
+
+   function addCompletedCourses(completedCourses){
+     var $badges = $('#badges');
+     completedCourses.forEach(function(item){
+       $('<div /> ',{
+         'class': 'course'}
+       ).appentTo('$badges');
+     });
+   }
 
 });
