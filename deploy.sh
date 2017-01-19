be398e3d120c8e2bfcfed7f01deaaffec559e668
seoige1 <seoige88@gmail.com>
2017-01-19 09:40:53 +0000

    update deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Welcome to Autodeploy - This will commit and push your latest changes"
+echo -n "Enter a commit message > "
+read msg
+echo "You entered: $msg"
+git st
+echo ""
+echo "..............."
+echo ""
+git add *
+git st
+echo ""
+echo "................"
+echo ""
+git commit -m "$msg"
+git st
+echo ""
+echo "................"
+echo ""
+git push
+git st
+echo ""
+echo "................"
+echo ""
+echo "deploying to git pages:"
+git subtree push --prefix src origin gh-pages
+echo "done!"
